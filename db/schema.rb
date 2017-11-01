af45a280b51f6a3c46bef316563951bd2be4af0c
Yang-Hsiang Chang <leo424y@gmail.com>
2017-11-01 17:27:57 +0800

    routes model

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171101084434) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "logs", force: :cascade do |t|
     t.string "area"
     t.integer "count"
