5b2feb76c67a066daad260ef6ebf4ca1dec052b2
Yang-Hsiang Chang <leo424y@gmail.com>
2017-11-03 12:59:13 +0800

    .

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ post '/callback' do
         Log.create(area: m[0], count: m[1])
 
         reply = case m[0]
-        when '西' then "你提供的資訊是：在#{m[0]}區發現#{m[1]}。謝謝你，我收到囉！想知道更多#{m[0]}區情報？請輸入：#{m[0]}區 #{Log.all}"
+        when '西' then "你提供的資訊是：在#{m[0]}區發現#{m[1]}。謝謝你，我收到囉！想知道更多#{m[0]}區情報？請輸入：#{m[0]}區 #{Log.where(area: m[0]+'區').count}"
         end
 
         message = {
