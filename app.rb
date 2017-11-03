6e909fecc2eb4f4f29dd378d44d187aa01f1bb7f
Yang-Hsiang Chang <leo424y@gmail.com>
2017-11-03 22:12:09 +0800

    .

--- app.rb
+++ app.rb
@@ -30,9 +30,15 @@ post '/callback' do
             "#{m[0]}區 #{Log.where(area: m[0]).order(id: :desc).pluck(:info)}"
           end
         when /我跑了/ then
+          all_number = Log.group(:info).count.keys
+          all_msg = ''
+          all_number.each do |a|
+            all_msg << "達#{a}單共#{Log.group(:info).count[a]}人"
+          end
+
           run_number = m.gsub(/[^0-9]/, '')
           Log.create(area: '跑單', info: run_number)
-          "目前跑#{run_number}單的伙伴共有#{Log.where(info: run_number).count}人，大家實在是太拼了，加油！送餐平安，日日平安"
+          "目前跑#{run_number}單的伙伴共有#{Log.where(info: run_number).count}人#{all_msg}。大家實在是太拼了，加油！送餐平安，日日平安"
         when /你好/ then "😄"
         when /車禍/ then
           tips = Log.where("info LIKE ?", "%車禍%")
