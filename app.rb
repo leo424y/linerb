1192f29f93be3d636a631e6a75bb3c6ce0e1c92a
Yang-Hsiang Chang <leo424y@gmail.com>
2017-11-03 20:47:04 +0800

    .

--- app.rb
+++ app.rb
@@ -29,20 +29,16 @@ post '/callback' do
             Log.create(area: m[0], info: m[1])
             "#{m[0]}區 #{Log.where(area: m[0]).order(id: :desc).pluck(:info)}"
           end
-        when /單/ then "😄"
+        when /跑了/ then
+          run_number = m.gsub(/[^0-9]/, '')
+          Log.create(area: '跑單', info: run_number)
+          "目前跑#{run_number}單的伙伴，共有#{Log.where(info: run_number).count}人了，實在是太拼了，加油！"
         when /你好/ then "😄"
         when /車禍/ then
           tips = Log.where("info LIKE ?", "%車禍%")
           "#{tips.pluck(:area)}區有車禍資訊，請小心#{tips.pluck(:info)}"
         else
-          run_number = m.gsub(/[^0-9]/, '')
-          case run_number
-          when Integer
-            Log.create(area: '跑單', info: run_number)
-            "目前跑#{run_number}單的伙伴，共有#{Log.where(info: run_number).count}人了，實在是太拼了，加油！"
-          else
-            "請輸入OO區XXX(天候路況店家等有益大家的情報) ex. 東西南北區下大雨 可一併看該區其它情報"
-          end
+          "請輸入OO區XXX(天候路況店家等有益大家的情報) ex. 東西南北區下大雨 可一併看該區其它情報"
         end
 
         message = {
