75ef04ac5c7b5611fb78cdfe010e85b4c01b6cb8
Yang-Hsiang Chang <leo424y@gmail.com>
2017-11-04 09:59:43 +0800

    .

--- app.rb
+++ app.rb
@@ -45,7 +45,7 @@ post '/callback' do
           tips = Log.where("info LIKE ?", "%車禍%")
           "#{tips.pluck(:area)}區有車禍資訊，請小心#{tips.pluck(:info)}"
         else
-          "請輸入OO區XXX(天候路況店家等有益大家的情報) ex. 東西南北區下大雨 可一併看該區其它情報"
+          "🍌請餵我【OO區X】(X是天候路況店家等情報) 例：東西南北區下大雨 可一併看該區其他情報🍌"
         end
 
         message = {
