75d073e48d8b75bf983e5795c2031034cf086ab9
Yang-Hsiang Chang <leo424y@gmail.com>
2017-11-07 17:50:54 +0800

    today number

--- app.rb
+++ app.rb
@@ -36,7 +36,7 @@ post '/callback' do
           if m[1]
             Log.create(area: m[0], info: m[1])
 
-            "#{m[0]}區 #{Log.where(area: m[0]).where('created_at >= ?', (Time.now - 60*60*24) ).order(id: :desc).pluck(:info).join('🚴')}"
+            "#{m[0]}區 #{Log.where(area: m[0]).where('created_at >= ?', (Time.now - 60*60*24*3) ).order(id: :desc).pluck(:info).join('🚴')}"
           end
         when /我覺得/ then
           m = m.split(%r{我覺得\s*})
@@ -48,16 +48,16 @@ post '/callback' do
           run_number = m.gsub(/[^0-9]/, '')
           Log.create(area: '跑單數', info: run_number)
 
-          all_number = Log.where(area: '跑單數').group(:info).count.keys
+          all_number = Log.where(area: '跑單數').where('created_at >= ?', (Time.now - 60*60*24*1)).group(:info).count.keys
           all_msg = ''
           all_number.each do |a|
-            all_msg << ("達#{a}單#{Log.group(:info).count[a]}人🚴")
+            all_msg << ("達#{a}單#{Log.where('created_at >= ?', (Time.now - 60*60*24*1)).group(:info).count[a]}人🚴")
           end
 
-          "目前累計跑#{run_number}單的猴主人共有#{Log.where(info: run_number).count}人。#{all_msg} 讓🐵優猴繼續為你加油！🚴送餐平安，日日平安🚴"
+          "今天累計跑#{run_number}單的猴主人共有#{Log.where('created_at >= ?', (Time.now - 60*60*24*1)).where(info: run_number).count}人。#{all_msg} 讓🐵優猴繼續為你加油！🚴送餐平安，日日平安🚴"
         when /你好/ then "😄"
         when /車禍/ then
-          tips = Log.where("info LIKE ?", "%車禍%")
+          tips = Log.where("info LIKE ?", "%車禍%").where('created_at >= ?', (Time.now - 60*60*24*1))
           "#{tips.pluck(:area)}區有車禍資訊，請小心#{tips.pluck(:info)}"
         else
           "🍌請餵我【OO區X】(X是天候路況店家等情報) 例：東西南北區下大雨 可一併看該區其他情報 🍌餵我【我跑了(數量)單】看看多少夥伴與你一樣拼🍌餵我【全區加油】為奔波的自己與彼此打氣！"
