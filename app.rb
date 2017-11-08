d04c1b84000e2c75cc858cbec1d20141134b1d9a
Yang-Hsiang Chang <leo424y@gmail.com>
2017-11-08 08:42:58 +0800

    space more

--- app.rb
+++ app.rb
@@ -29,7 +29,7 @@ post '/callback' do
         Place.create(address: l)
         message = {
           type: 'text',
-          text: "#{area}區 #{Log.where(area: area).where('created_at >= ?', (Time.now - 60*60*24*7) ).order(id: :desc).pluck(:info).join('🚴')}"
+          text: "#{area}區 #{Log.where(area: area).where('created_at >= ?', (Time.now - 60*60*24*7) ).order(id: :desc).pluck(:info).join(' 🚴 ')}"
         }
 
         client.reply_message(event['replyToken'], message)
@@ -47,13 +47,13 @@ post '/callback' do
           if m[1]
             Log.create(area: m[0], info: m[1])
 
-            "#{m[0]}區 #{Log.where(area: m[0]).where('created_at >= ?', (Time.now - 60*60*24*7) ).order(id: :desc).pluck(:info).join('🚴')}"
+            "#{m[0]}區 #{Log.where(area: m[0]).where('created_at >= ?', (Time.now - 60*60*24*7) ).order(id: :desc).pluck(:info).join(' 🚴 ')}"
           end
         when /我覺得/ then
           m = m.split(%r{我覺得\s*})
           if m[1]
             Log.create(area: '我覺得', info: m[1])
-            "猴主人們最近覺得：#{Log.where(area: '我覺得').order(id: :desc).pluck(:info).join('🚴')}"
+            "猴主人們最近覺得：#{Log.where(area: '我覺得').order(id: :desc).pluck(:info).join(' 🚴 ')}"
           end
         when /鴿子/ then
             "猴主人回報這些地方有鴿子：#{Log.where(area: '鴿子').order(id: :desc).pluck(:info).join('🐦')}"
@@ -64,23 +64,23 @@ post '/callback' do
           all_number = Log.where(area: '跑單數').where('created_at >= ?', (Time.now - 60*60*24*1)).group(:info).count.keys
           all_msg = ''
           all_number.each do |a|
-            all_msg << ("達#{a}單#{Log.where('created_at >= ?', (Time.now - 60*60*24*1)).group(:info).count[a]}人🚴")
+            all_msg << ("達#{a}單#{Log.where('created_at >= ?', (Time.now - 60*60*24*1)).group(:info).count[a]}人 🚴 ")
           end
 
-          "今天累計跑#{run_number}單的猴主人共有#{Log.where('created_at >= ?', (Time.now - 60*60*24*1)).where(info: run_number).count}人。#{all_msg} 讓🐵優猴繼續為你加油！🚴送餐平安，日日平安🚴"
+          "一天內累計跑#{run_number}單的猴主人共有#{Log.where('created_at >= ?', (Time.now - 60*60*24*1)).where(info: run_number).count}人。#{all_msg} 讓🐵優猴繼續為你加油！ 🚴 送餐平安，日日平安 🚴 "
         when /你好/ then "😄"
         when /車禍/ then
           tips = Log.where("info LIKE ?", "%車禍%").where('created_at >= ?', (Time.now - 60*60*24*1))
           "#{tips.pluck(:area)}區有車禍資訊，請小心#{tips.pluck(:info)}"
         else
           [
-            "🍌餵我【OO區X】(X是天候路況店家等情報) 例：東西南北區下大雨 可一併看該區其他情報",
-            "🍌餵我【我跑了(數量)單】看看多少夥伴與你一樣拼",
-            "🍌餵我【全區加油】為奔波的自己與彼此打氣！",
-            "🍌餵我【全區開跑】宣告今天即將爆單！",
-            "🍌餵我【我覺得(感受)】抒發心情或給猴子管理員建議，也看看其他主人們心情如何。",
-            "🍌餵我【鴿子在...】與【鴿子】分別能回報與查詢牠們，避免一天的辛苦成為飼料。🐦",
-            "🌏按左下➕號分享位置資訊可查詢附近情報",
+            "🍌 餵我【OO區X】(X是天候路況店家等情報) 例：東西南北區下大雨 可一併看該區其他情報",
+            "🍌 餵我【我跑了(數量)單】看看多少夥伴與你一樣拼",
+            "🍌 餵我【全區加油】為奔波的自己與彼此打氣！",
+            "🍌 餵我【全區開跑】宣告今天即將爆單！",
+            "🍌 餵我【我覺得(感受)】抒發心情或給猴子管理員建議，也看看其他主人們心情如何。",
+            "🍌 餵我【鴿子在...】與【鴿子】分別能回報與查詢牠們，避免一天的辛苦成為飼料。🐦",
+            "🌏 按左下➕號分享位置資訊可查詢附近情報",
           ].shuffle.first
         end
 
