15cc38c0cdd72ee675ab59422cbb98a26171616e
leo424y <leo424y@gmail.com>
2018-07-28 14:56:40 +0800

    nil

--- app.rb
+++ app.rb
@@ -49,10 +49,10 @@ post '/callback' do
           begin
             formatted_phone_number = ''
             place_id = doc['candidates'][0]['place_id']
-            if place_id != nil
+            unless place_id.nil?
               place_id_url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=#{place_id}&fields=name,rating,formatted_phone_number&key=#{gmap_key}"
               place_id_doc = JSON.parse(open(place_id_url).read, :headers => true)
-              formatted_phone_number = "  📞 #{place_id_doc['result']['formatted_phone_number'].gsub(" ","")}"
+              formatted_phone_number = "  📞 #{place_id_doc['result']['formatted_phone_number'].gsub(" ","")}" unless place_id_doc['result']['formatted_phone_number'].nil?
             end
             rating = (doc['candidates'][0]['rating'].to_f * 2).to_i
             star = '⭐'* (rating/2)+'✨' * (rating%2)
