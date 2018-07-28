e4a70f3bc54897fa41d784d06345a4ee6884b667
leo424y <leo424y@gmail.com>
2018-07-28 14:44:18 +0800

    phone!

--- app.rb
+++ app.rb
@@ -52,7 +52,7 @@ post '/callback' do
             if place_id
               place_id_url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=#{place_id}&fields=name,rating,formatted_phone_number&key=#{gmap_key}"
               place_id_doc = JSON.parse(open(place_id_url).read, :headers => true)
-              formatted_phone_number = "電話：#{place_id_doc['result']['formatted_phone_number'].strip}"
+              formatted_phone_number = "電話：#{place_id_doc['result']['formatted_phone_number'].gsub(" ","")}"
             end
             rating = (doc['candidates'][0]['rating'].to_f * 2).to_i
             star = '⭐'* (rating/2)+'✨' * (rating%2)
