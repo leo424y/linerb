438d38800621636aa91ec3c16d79cc544a33a85a
leo424y <leo424y@gmail.com>
2018-08-05 13:09:03 +0800

    api

--- bin/bitly.rb
+++ bin/bitly.rb
@@ -1,7 +1,7 @@
 require 'bitly'
 NAME='leo424yy'
-# API_KEY='R_c545a03a3cbf4034b390fa5c4c153cb2'
-API_KEY='R_9def63bcade34721826109d5dcd98fb1'
+API_KEY='R_c545a03a3cbf4034b390fa5c4c153cb2'
+# API_KEY='R_9def63bcade34721826109d5dcd98fb1'
 Bitly.use_api_version_3
 
 Bitly.configure do |config|
