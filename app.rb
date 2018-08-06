20ddfab64761e855e4a9c44f5901d3349b9c57ea
leo424y <leo424y@gmail.com>
2018-08-06 17:09:06 +0800

    pocket go

--- app.rb
+++ app.rb
@@ -121,10 +121,14 @@ post '/callback' do
         if name.end_with?('放口袋')
           if in_vip
             Pocket.create(user_id: user_id, place_name: name.chomp('放口袋'))
-            message = "👜 已將#{name}放口袋"
+            message_text = "👜 已將#{name}放口袋"
           else
-            message = '🥇 請先升級就能放口袋囉'
+            message_text = '🥇 請先升級就能放口袋囉'
           end
+          message = {
+            type: 'text',
+            text: message_text
+          }
           client.reply_message(event['replyToken'], message)
         elsif m.end_with?(*suffixes) && (name != '') && (name.bytesize < 40)
           actions_a = [
