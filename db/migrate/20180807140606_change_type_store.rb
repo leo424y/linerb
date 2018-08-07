aec31812d60014b358d25f56c6f748c685c24a7a
leo424y <leo424y@gmail.com>
2018-08-07 17:39:30 +0800

    .

--- db/migrate/20180807140606_change_type_store.rb
+++ db/migrate/20180807140606_change_type_store.rb
@@ -1,6 +1,6 @@
 class ChangeTypeStore < ActiveRecord::Migration[5.1]
   def change
-    change_column :stores, :lat, :decimal
-    change_column :stores, :lng, :decimal
+    change_column :stores, :lat, :decimal, using: 'lag::decimal'
+    change_column :stores, :lng, :decimal, using: 'lng::decimal'
   end
 end
