02289aaa57ad86913db70eeca690e0847130fefb
leo424y <leo424y@gmail.com>
2018-08-09 22:18:48 +0800

    pocket

--- app.rb
+++ app.rb
@@ -140,7 +140,7 @@ def handle_location(event, user_id)
   my_lat = message['latitude'].to_s[0..4]
   my_lng = message['longitude'].to_s[0..5]
   my_store = Store.where("lat like ?", "#{my_lat}%").where("lng like ?", "#{my_lng}%")
-  results = my_store.pluck(:name_sys).first
+  results = my_store.pluck(:name_sys).uniq[0..3]
   result_message = results.empty? ? "🗽 附近尚無開民蹤影，趕快來當第一吧！" : "🎐 附近開民怕落空的地點有..."
   Position.create(user_id: user_id, lat: message['latitude'], lng: message['longitude'])
   actions_a = results.map do |result|
@@ -202,11 +202,7 @@ def handle_message(event, user_id, in_vip, group_id, is_group)
       else
         message_text = '🥇 請先升級就能放口袋囉'
       end
-      message = {
-        type: 'text',
-        text: message_text
-      }
-      client.reply_message(event['replyToken'], message)
+      reply_text(event, message_text)
 
     elsif m.end_with?(*suffixes) && (name != '') && (name.bytesize < 40)
       s_link = %x(ruby bin/bitly.rb '#{link}').chomp
