1213c262cf65017f339412fef7b9a3cc66ed9915
leo424y <leo424y@gmail.com>
2018-08-09 23:16:18 +0800

    .

--- app.rb
+++ app.rb
@@ -84,28 +84,13 @@ def handle_location(event, user_id)
   my_lat = message['latitude'].to_s[0..4]
   my_lng = message['longitude'].to_s[0..5]
   my_store = Store.where("lat like ?", "#{my_lat}%").where("lng like ?", "#{my_lng}%")
-  results = my_store.pluck(:name_sys).uniq[0..3]
+  results = my_store.pluck(:name_sys).uniq
   result_message = results.empty? ? "🗽 附近尚無開民蹤影，趕快來當第一吧！" : "🎐 附近開民怕落空的地點有..."
   Position.create(user_id: user_id, lat: message['latitude'], lng: message['longitude'])
   msg = []
   msg << result_message
-  results.map { |r| msg << "#{r.join(', ')}"}
-  # actions_a = results.map do |result|
-  #   {
-  #     type: 'uri', label: "📍 #{result}", uri: "https://www.google.com/maps/search/?api=1&query=#{result}"
-  #   }
-  # end
-  # message_buttons = {
-  #   type: 'template',
-  #   altText: '...',
-  #   template: {
-  #     type: 'buttons',
-  #     title: '開民雷達',
-  #     text: result_message,
-  #     actions: actions_a.compact
-  #   }
-  # }
-  # reply_content(event, message_buttons)
+  msg << results.join(' ')
+
   reply_text(event, msg)
 end
 
