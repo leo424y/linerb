c01b8888bdfd1f40d706ba66711e7006359fb763
leo424y <leo424y@gmail.com>
2018-08-09 23:07:31 +0800

    .

--- app.rb
+++ app.rb
@@ -84,7 +84,7 @@ def handle_location(event, user_id)
   my_lat = message['latitude'].to_s[0..4]
   my_lng = message['longitude'].to_s[0..5]
   my_store = Store.where("lat like ?", "#{my_lat}%").where("lng like ?", "#{my_lng}%")
-  results = my_store.pluck(:name_sys).uniq
+  results = my_store.pluck(:name_sys).uniq[0..3]
   result_message = results.empty? ? "🗽 附近尚無開民蹤影，趕快來當第一吧！" : "🎐 附近開民怕落空的地點有..."
   Position.create(user_id: user_id, lat: message['latitude'], lng: message['longitude'])
   # msg = []
