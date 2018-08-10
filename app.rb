16f4ca10c777884dc52eb97a4ad27231dbc041f9
leo424y <leo424y@gmail.com>
2018-08-10 09:06:54 +0800

    gogo near

--- app.rb
+++ app.rb
@@ -74,22 +74,21 @@ def handle_join(event, group_id)
   reply_text(event, IO.readlines("data/join").map(&:chomp))
 end
 
-def handle_location(event, user_id)
-  message = event.message
-  my_lat = message['latitude'].to_s[0..4]
-  my_lng = message['longitude'].to_s[0..5]
-  my_store = Store.where("lat like ?", "#{my_lat}%").where("lng like ?", "#{my_lng}%")
-  results = my_store.pluck(:name_sys).uniq[0..2]
+def handle_location(event, user_id, lat, lng)
+  results = handle_nearby(lat, lng)
   result_message = results.empty? ? "🗽 附近尚無開民蹤影，趕快來當第一吧！" : "🎐 附近開民怕落空的地點有..."
-
-  Position.create(user_id: user_id, lat: message['latitude'], lng: message['longitude'])
-  # reply_text(event, [result_message, results.join("\n")])
-
   actions_a = results.map { |r|
     { label: "📍 #{r}" , type: 'message', text: "#{r}有開嗎？" }
   }.compact
-
+  Position.create(user_id: user_id, lat: lat, lng: lng)
   reply_content(event, message_buttons_h('開民雷達', result_message, actions_a))
+end
+
+def handle_nearby lat, lng
+  my_lat = lat.to_s[0..4]
+  my_lng = lng.to_s[0..5]
+  my_store = Store.where("lat like ?", "#{my_lat}%").where("lng like ?", "#{my_lng}%")
+  my_store.pluck(:name_sys).uniq[0..2]
 end
 
 def handle_message(event, user_id, is_vip, group_id)
@@ -97,7 +96,7 @@ def handle_message(event, user_id, is_vip, group_id)
 
   case event.type
   when Line::Bot::Event::MessageType::Location
-    group_id ? handle_location(event, user_id) : reply_text(event, '請於群組中使用')
+    group_id ? handle_location(event, user_id, event.message['latitude'], event.message['longitude']) : reply_text(event, '請於群組中使用')
 
   when Line::Bot::Event::MessageType::Text
     suffixes = IO.readlines("data/keywords").map(&:chomp)
