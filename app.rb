826796d02c215e6794d9a694003bcabebcb45bb8
leo424y <leo424y@gmail.com>
2018-08-13 12:34:36 +0800

    .

--- app.rb
+++ app.rb
@@ -136,7 +136,7 @@ def handle_message(event, user_id, is_vip, group_id)
 
     elsif name.end_with?('口袋有洞')
       pocket = Pocket.where(user_id: user_id).pluck(:place_name)[-4..-1]
-      if !pocket
+      if pocket
         actions_a = pocket.map { |p|
           {label: "📍 #{p}", type: 'uri', uri: "#{GG_SEARCH_URL}#{URI.escape(p)}"}
         }
