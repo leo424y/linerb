05caa99c1444bef54b5851114c884d0e327fe656
leo424y <leo424y@gmail.com>
2018-08-16 16:33:09 +0800

    .

--- app.rb
+++ app.rb
@@ -382,7 +382,7 @@ def number_to_cost_h user_id, place_info, cost
       text: "確認在#{place_info[1]}花了#{cost}元？",
       actions: [
         { label: '是的', type: 'postback', data: "book/#{user_id}/#{place_info[0]}/#{place_info[1]}/#{cost}"},
-        { label: '沒有', type: 'message', text: '好的沒事' },
+        { label: '沒有', type: 'message', text: '沒有' },
       ],
     }
   }
