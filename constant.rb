59c02b74c57612c785f7b8d66d58a1220785bcb1
leo424y <leo424y@gmail.com>
2018-08-21 21:03:37 +0800

    constant

---
+++ constant.rb
@@ -0,0 +1,7 @@
+GG_SEARCH_URL = "https://www.google.com/maps/search/?api=1&query="
+GG_FIND_URL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
+GG_DETAIL_URL = 'https://maps.googleapis.com/maps/api/place/details/json'
+GMAP_KEY = ENV["GMAP_API_KEY"]
+L_OPINION_URI = 'line://home/public/post?id=gxs2296l&postId=1153267270308077285'
+L_RECOMMEND_URI = "line://nv/recommendOA/@gxs2296l"
+L_SPONSOR_URI = 'http://j.mp/open_sponsor'
