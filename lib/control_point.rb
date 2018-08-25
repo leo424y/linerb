f00292a1ef26c1f348a014b038b5eb47814f3f84
leo424y <leo424y@gmail.com>
2018-08-25 10:37:50 +0800

    .

--- lib/control_point.rb
+++ lib/control_point.rb
@@ -1,7 +1,7 @@
 def add_point user_id, group_id, points
   Point.create(user_id: user_id, group_id: group_id, points: points)
   user = User.find_or_create_by(user_id: user_id)
-  user.update(points: user.points + point)
+  user.update(points: user.points + points)
   group = Group.find_by(group_id: group_id)
-  group.update(points: group.points + point) if group
+  group.update(points: group.points + points) if group
 end
