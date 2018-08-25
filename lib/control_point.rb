723f2cad13d75dba9edb76eff263cc0f0e1c19d1
leo424y <leo424y@gmail.com>
2018-08-25 14:37:47 +0800

    Today time!

--- lib/control_point.rb
+++ lib/control_point.rb
@@ -1,5 +1,5 @@
 def add_point user_id, group_id, points
-  todays_point_count = Point.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).where(user_id: user_id).count
+  todays_point_count = Point.where(created_at: Date.today.beginning_of_day..Date.today.end_of_day).where(user_id: user_id).count
   if todays_point_count > 6
     Point.create(user_id: user_id, group_id: group_id, points: points)
     user = User.find_or_create_by(user_id: user_id)
