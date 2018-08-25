211886167612f0762f26f69a01d516ac82bcf5f8
leo424y <leo424y@gmail.com>
2018-08-25 15:32:04 +0800

    .

--- lib/control_point.rb
+++ lib/control_point.rb
@@ -1,6 +1,9 @@
 def add_point user_id, group_id, points
-  todays_point_count = Point.where(created_at: Date.today.beginning_of_day..Date.today.end_of_day).where(user_id: user_id).where.not(points: 3).count
+  today_point = Point.where(created_at: Date.today.beginning_of_day..Date.today.end_of_day)
+  user_today_point = today_point.where(user_id: user_id)
+  todays_point_count = user_today_point.where.not(points: 3).count
   Point.create(user_id: user_id, group_id: group_id, points: points)
+  p "#{today_point}\n#{user_today_point}\n#{todays_point_count}"
   if todays_point_count < 30
     user = User.find_or_create_by(user_id: user_id)
     user.update(points: user.points + points)
