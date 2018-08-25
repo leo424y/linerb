5fce4c74db47a25b8ee4a7892af11ab1720eebda
leo424y <leo424y@gmail.com>
2018-08-25 10:52:49 +0800

    skip me

--- lib/handle_button.rb
+++ lib/handle_button.rb
@@ -9,7 +9,7 @@ def handle_button place_id, name, s_link
     { label: '👼 贊助', type: 'uri', uri: L_SPONSOR_URI }
   when 3
     king_points = User.maximum(:points)
-    king_users = User.where(points: king_points).pluck(:user_id)
+    king_users = User.where(points: king_points).pluck(:user_id)-['Ubee12276c9fb5bbb3989329e80587244']
     king_user_name = king_users.map{|n| user_name n}.join(' ')
     { label: '👑 名人堂', type: 'postback', data: "#{king_user_name}" }
   end
