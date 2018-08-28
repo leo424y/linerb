123526fec3b24e654da88e4a924588d2825bc5c7
leo424y <leo424y@gmail.com>
2018-08-28 22:15:31 +0800

    .

--- lib/text_.rb
+++ lib/text_.rb
@@ -1,6 +1,6 @@
 def handle_text event, user_id, group_id, origin_message
   suffixes = IO.readlines("data/keywords").map(&:chomp)
-  clean_message = origin_message.downcase.delete(" .。，,?？\t\r\n")
+  clean_message = origin_message.downcase.delete(" .。，,?？！\t\r\n")
   name = clean_message.chomp('嗎').chomp('有沒有開').chomp('開了沒').chomp('有開').chomp('開了').chomp('は開いていますか').chomp('現在').chomp('附近').chomp('推薦').chomp('放口袋')
 
   if origin_message.end_with?('附近', '推薦有開嗎', '放口袋', '？！')
