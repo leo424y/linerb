9ded2dd9b536bd3f81b79d5f963171d4c297ff5e
leo424y <leo424y@gmail.com>
2018-08-29 13:58:26 +0800

    .

--- lib/api_wiki.rb
+++ lib/api_wiki.rb
@@ -1,7 +1,7 @@
 def wiki_content event, name
   wiki_data = wikir(name, 'zh')
   if wiki_data.text
-    wiki_data.text.truncate(200) + %x(ruby bin/bitly.rb "#{wiki_data.fullurl}").chomp
+    "#{wiki_data.text.truncate(200)} " + %x(ruby bin/bitly.rb "#{wiki_data.fullurl}").chomp
   else
     nil
   end
