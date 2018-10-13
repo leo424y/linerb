a18f46dd03fbf7f26f57ad09912e33de12c1ffd9
leo424y <leo424y@gmail.com>
2018-10-13 22:30:54 +0800

    msgs go

--- lib/c_issue.rb
+++ lib/c_issue.rb
@@ -1,11 +1,18 @@
 def open_issue event, user_id, group_id, origin_message
   tag = origin_message.end_with?('意見') ? 'idea' : 'issue'
   issue = Issue.where(user_id: user_id, group_id: group_id, tag: tag).last(3).pluck(:title, :refs)
+  p issue
   if issue
-    actions_a = issue.map { |p|
-      {label: "🙋 #{p[0][0..12]}...", type: 'uri', uri: "http://#{(p[1])}"}
-    }
-    reply_content(event, message_buttons_h(origin_message, '裡頭最新三則為', actions_a) )
+    msgs = []
+    issue.each do |p|
+      msgs << "🙋 #{p[0][0..12]}... http://#{(p[1])}"
+      p msgs
+    end
+    # actions_a = issue.map { |p|
+    #   {label: "🙋 #{p[0][0..12]}...", type: 'uri', uri: "http://#{(p[1])}"}
+    # }
+    # reply_content(event, message_buttons_h(origin_message, '裡頭最新三則為', actions_a) )
+    reply_text(event, msgs)
   else
     reply_text(event, '😶 居民尚無回應')
   end
