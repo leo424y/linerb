7c09f68c0442a8090f472f7c53b71ef98f37a99e
leo424y <leo424y@gmail.com>
2018-10-13 22:20:50 +0800

    no need escape

--- lib/c_issue.rb
+++ lib/c_issue.rb
@@ -3,7 +3,7 @@ def open_issue event, user_id, group_id, origin_message
   issue = Issue.where(user_id: user_id, group_id: group_id, tag: tag).last(3).pluck(:title, :refs)
   if issue
     actions_a = issue.map { |p|
-      {label: "🙋 #{p[0][0..12]}...", type: 'uri', uri: "http://#{URI.escape(p[1])}"}
+      {label: "🙋 #{p[0][0..12]}...", type: 'uri', uri: "http://#{(p[1])}"}
     }
     reply_content(event, message_buttons_h(origin_message, '裡頭最新三則為', actions_a) )
   else
