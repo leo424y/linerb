60b13e9508a97bc273d7642083b6fefe64343b39
leo424y <leo424y@gmail.com>
2018-10-18 16:07:27 +0800

    name array

--- lib/text_end_with.rb
+++ lib/text_end_with.rb
@@ -51,7 +51,7 @@ def handle_text_end_with event, user_id, group_id, origin_message, name
     if my_place
       gamers = update_game user_id, group_id, my_place.place_name
       gamer_names = []
-      gamer_names << gamers.each {|x| name_user x}
+      gamers.each {|x| gamer_names << name_user(x)}
       reply_text(event, "#{place_name}團加一成功，#{gamer_names.join(', ')}已參加")
     else
       reply_text event, "請先搜尋想去的地點+有開嗎！"
