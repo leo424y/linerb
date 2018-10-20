1dd923e5c4fbcbfa4d3d43afb8432e437a50081d
leo424y <leo424y@gmail.com>
2018-10-20 11:05:35 +0800

    gogo map

--- lib/c_game.rb
+++ lib/c_game.rb
@@ -1,5 +1,4 @@
 def new_game event, user_id, group_id, place_name
-  p 'new_game'
   Game.create(
     user_id: user_id,
     group_id: group_id,
@@ -8,7 +7,8 @@ def new_game event, user_id, group_id, place_name
   reply_content(event, message_buttons_h(
     "#{place_name}開團", '來加一吧！',
     [
-      { label: '☝️ 加一', type: 'message', text: "#{place_name}+1" }
+      { label: '☝️ 加一', type: 'message', text: "#{place_name}+1" },
+      { label: '📍 位置', type: 'uri', uri: URI.escape("#{GG_SEARCH}#{place_name}") }
     ]))
   # reply_content(event, message_buttons_h(
   #   "#{place_name}團", '請選預計時間',
