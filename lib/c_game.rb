e12f18d4968ff20f6a01dd5b08a0630bf7d8d8ae
leo424y <leo424y@gmail.com>
2018-10-22 10:10:19 +0800

    ++1

--- lib/c_game.rb
+++ lib/c_game.rb
@@ -1,11 +1,11 @@
 def new_game event, user_id, group_id, place_name
-  Game.create(
+  Game.find_or_create_by(
     user_id: user_id,
     group_id: group_id,
     place_name: place_name
   )
   reply_content(event, message_buttons_h(
-    "#{place_name}開團", '來加加一吧！',
+    "#{place_name}有開團", '來加加一吧！',
     [
       { label: '☝️ ++1', type: 'message', text: "#{place_name}++1" },
       { label: '📍 位置', type: 'uri', uri: URI.escape("#{GG_SEARCH}#{place_name}") }
