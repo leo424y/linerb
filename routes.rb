1ca0d8fa23c9298478feda78f1c80a6a386ad21e
leo424y <leo424y@gmail.com>
2018-10-24 15:02:45 +0800

    .

--- routes.rb
+++ routes.rb
@@ -2,33 +2,23 @@ current_dir = Dir.pwd
 Dir["#{current_dir}/models/*.rb"].each { |file| require file }
 
 get '/logs' do
-
   @logs = Log.all
-
 end
 
 
 get '/logs/:id' do
-
   @log = Log.find(params[:id])
-
 end
 
 post '/logs' do
-
   @log = Log.create(params[:log])
-
 end
 
 put '/logs/:id/publish' do
-
   @log = Log.find(params[:id])
   @log.publish!
-
 end
 
 delete '/logs/:id' do
-
   Log.destroy(params[:id])
-
 end
