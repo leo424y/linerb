70f3a504eaa9ca4c438033f0f3bb5a0729bde7ab
leo424y <leo424y@gmail.com>
2018-10-25 16:46:54 +0800

    flex it!

--- lib/c_game.rb
+++ lib/c_game.rb
@@ -4,12 +4,60 @@ def new_game event, user_id, group_id, place_name
     group_id: group_id,
     place_name: place_name
   )
-  reply_content(event, message_buttons_h(
-    '揪揪團！來加加一吧！', "#{place_name}開團", 
-    [
-      { label: '☝️ ++1', type: 'message', text: "#{place_name}++1" },
-      { label: '📍 位置', type: 'uri', uri: URI.escape("#{GG_SEARCH}#{place_name}") }
-    ]))
+  reply_content(event, {
+    type: "flex",
+    altText: "this is a flex message",
+    contents: {
+      type: "bubble",
+      header: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          {
+            type: "text",
+            text: "揪揪團！來++1"
+          }
+        ]
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          {
+            type: "text",
+            text: "#{place_name}",
+          }
+        ]
+      },
+      footer: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          {
+            type: "button",
+            style: "primary",
+            action: [{
+              type: "message",
+              label: "☝️ ++1",
+              uri: "#{place_name}++1"
+            },
+            {
+              type: 'uri',
+              label: '📍 位置',
+              uri: URI.escape("#{GG_SEARCH}#{place_name}")
+            }
+          ]
+          }
+        ]
+      }
+    }
+  })
+  # reply_content(event, message_buttons_h(
+  #   '揪揪團！來加加一吧！', "#{place_name}開團",
+  #   [
+  #     { label: '☝️ ++1', type: 'message', text: "#{place_name}++1" },
+  #     { label: '📍 位置', type: 'uri', uri: URI.escape("#{GG_SEARCH}#{place_name}") }
+  #   ]))
 end
 
 def update_game user_id, group_id, place_name
