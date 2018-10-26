f1d53917c693228bef5e7ad8a0c3fb03c56fc6b9
leo424y <leo424y@gmail.com>
2018-10-26 15:35:16 +0800

    array!

--- lib/c_game.rb
+++ lib/c_game.rb
@@ -15,7 +15,7 @@ end
 
 def reply_game event, place_name, more
   info_link = URI.extract(place_name)
-  place_name = place_name.gsub(info_link, '') if info_link
+  place_name = place_name.gsub(info_link[0], '') if info_link[0]
   reply_content(event, {
     type: "flex",
     altText: "this is a flex message",
